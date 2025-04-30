7edc7fbe21edb4403ddec75b662a97924fa93e93
Kartik-1902 <kartikdhingani0711@gmail.com>
2025-04-30 20:40:02 +0530

    i dont know what new i have added

--- Legend-Of-Porche/src/App.jsx
+++ Legend-Of-Porche/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './global.css';
 import HeroSection from './components/HeroSection';
-
+import CarSection from './components/CarSection';
 function App() {
   return (
     <div className="App font-sans text-white bg-black">
--- Legend-Of-Porche/src/components/HeroSection.jsx
+++ Legend-Of-Porche/src/components/HeroSection.jsx
@@ -6,6 +6,17 @@ export default function HeroSection() {
   const scope = useRef(null);
 
 	useEffect(() => {
+		const cursor = document.getElementById('custom-cursor');
+		const porscheText = document.getElementById('porsche-text');
+		
+		const moveCursor = (e) => {
+			const { clientX, clientY } = e;
+			cursor.style.transform = `translate(${clientX}px, ${clientY}px) translate(-60%, -60%)`;
+		};
+		
+
+    window.addEventListener('mousemove', moveCursor);
+		
 		scope.current = createScope({ rootRef }).add(self => {
 			
 			// Pulse Animation (independent)
@@ -16,13 +27,13 @@ export default function HeroSection() {
 				duration: 4000,
 			});
 			animate('.scroll-circle', {
-				translateY: [0, 20, 0],
-				opacity: [1, 0.6, 1],
+				translateY: [0, 40, 0],
+				opacity: [1, 0.4, 1],
 				easing: 'easeInOutSine',
 				duration: 1500,
 				loop: true,
 			}, '-=5000');
-			
+
 			// Fade overlay and move logo together
 			const transitionTimeline = createTimeline({ autoplay: false })
 				.add('.hero-overlay', {
@@ -31,8 +42,8 @@ export default function HeroSection() {
 					easing: 'easeOutQuad',
 				}, 0) 
 				.add('.porsche-text', {
-					top: '3vh',
-					left: '3vw',
+					top: '4vh',
+					left: '6vw',
 					scale: 0.5,
 					duration: 1000,
 					easing: 'easeOutQuad',
@@ -43,26 +54,27 @@ export default function HeroSection() {
 				.sync(pulse)
 				.sync(transitionTimeline, '+=0'); // start transition *immediately* after pulse ends
 	
-			return () => scope.current.revert();
+			return () => {scope.current.revert(),
+				window.removeEventListener('mousemove', moveCursor);
+			};
 		});
 	}, []);
 
 
   return (
     <div ref={rootRef} className="hero relative h-screen overflow-hidden">
-      <div
-        className="hero-image absolute inset-0 bg-cover bg-center z-0"
-        style={{ backgroundImage: `url('/porsche-targa-1621744_1920.jpg')` }}
-      />
+			<div  className="hero-image absolute inset-0 bg-cover bg-center z-0"
+      style={{ backgroundImage: `url('/porsche-targa-1621744_1920.jpg')` }}/>
       <div className="hero-overlay absolute inset-0 bg-black z-10" />
       <h1
-        className="porsche-text font-meaculpa absolute mt-[0.2rem]  ml-[0.2rem] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-6xl z-20"
+        className="porsche-text font-orbitron absolute mt-[0.2rem]  ml-[0.2rem] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-6xl z-20"
       >
         Porsche
       </h1>
 			<div className="scroll-indicator absolute mt-[0.2rem]  ml-[0.2rem] top-[95%] left-1/2 -translate-x-1/2 -translate-y-1/22">
-			<div class="scroll-circle  absolute  w-4 h-4 rounded-full bg-[#f68548]"></div>
+				<div class="scroll-circle  absolute  w-4 h-4 rounded-full bg-[#f68548]"></div>
 			</div>
+		  <div id="custom-cursor" className="pointer-events-none fixed top-0 left-0 w-8 h-8 border-2 border-[#f68548] rounded-full transform -translate-x-1/2 -translate-y-1/2 z-50"></div>
     </div>
   );
 }
